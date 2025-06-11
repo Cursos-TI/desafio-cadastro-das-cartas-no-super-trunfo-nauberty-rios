4c472b4f215e8ffbc53a718a4344608e4c23207a
NAUBERTY DE MORAES RIOS <naubertymoraes13@gmail.com>
2025-06-11 22:11:41 +0000

    Terminei mas deu pane no string do nome %s

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,86 @@
 #include <stdio.h>
+#include "CartasSuperTrunfo.h"
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
+
+
+// Software do jogo Super Trunfo
+// Passei boas madrugadas tentando resolver o porque quando eu declarava os nomes em %s string nao funcionava o codigo 
+//de resto esta tudo funcionando !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    int rank; 
+    char nome;
+    int populacao;
+    float area;
+    float pib;
+    int pturisticos;
 
-    return 0;
+    int rankseg; 
+    char nomeseg;
+    int populacaoseg;
+    float areaseg;
+    float pibseg;
+    int pturisticosseg;
+
+printf("Oque acha de cadastrar suas cartas comigo. Bom vamos la:\n" );
+printf("Cadastre o nome da cidade:\n ");
+scanf("%s", &nome);
+
+printf("Informe o numero de habitantes dessa cidade:\n ");
+scanf("%d", &populacao);
+
+printf("Informe a area que essa cidade cobre:\n ");
+scanf("%f", &area);
+
+printf("Informe o pib dessa cidade:\n ");
+scanf("%f", &pib);
+
+printf("Me diga quantos pontos turisticos tem nessa cidade:\n");
+scanf("%d", &pturisticos);
+
+printf("Vamos cadastrar a segunda carta!!\n\n");
+scanf("Nome da cidade é: %s\n", nomeseg);
+
+printf("Diga o nome da proxima cidade:\n");
+scanf("%s", &nomeseg);
+
+printf("Informe o numero de habitantes dessa cidade:\n ");
+scanf("%d", &populacaoseg);
+
+printf("Informe a area que essa cidade cobre:\n ");
+scanf("%f", &areaseg);
+
+printf("Informe o pib dessa cidade:\n ");
+scanf("%f", &pibseg);
+
+printf("Me diga quantos pontos turisticos tem nessa cidade:\n\n");
+scanf("%d", &pturisticosseg);
+
+printf("Cadastro concluido com sucesso!!\n\n");
+
+printf("Carta 1\n");
+printf("Estado: A\n");
+printf("Código: A01\n");
+printf("Nome da cidade: %d \n", nome);
+printf("população: %d \n", populacao);
+printf("Área: %f km² \n", area);
+printf("PIB %f bilhões de reais \n", pib);
+printf("Número de pontos turísticos: %d\n", pturisticos);
+
+printf("\n\nCarta 2\n");
+printf("Estado: B\n");
+printf("Código: B02\n");
+printf("Nome da cidade: %d \n", nomeseg);
+printf("população: %d \n", populacaoseg);
+printf("Área: %f km² \n", areaseg);
+printf("PIB %f bilhões de reais \n", pibseg);
+printf("Número de pontos turísticos: %d\n\n", pturisticosseg);
+return 0;
 }
