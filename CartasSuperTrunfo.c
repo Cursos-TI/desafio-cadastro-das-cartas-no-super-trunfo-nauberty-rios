1da376c91a4cf4b61c3afbfc5619a25722d8eb4f
NAUBERTY DE MORAES RIOS <naubertymoraes13@gmail.com>
2025-06-17 00:02:32 +0000

    terminado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -54,7 +54,7 @@ printf("Cadastro concluido com sucesso!!\n\n");
 rank1 = (populacao + area + pib + pturisticos + pibcapita); 
 rank2 = (populacaoseg + areaseg + pibseg + pturisticosseg + pibcapitaseg);
 
-/*printf("Rank: %f\n",rank1);
+printf("Rank: %f\n",rank1);
 printf("Carta 1\n");
 printf("Estado: A\n");
 printf("Código: A01\n");
@@ -64,10 +64,10 @@ printf("Área: %f km² \n", area);
 printf("PIB: %f bilhões de reais \n", pib);
 printf("Número de pontos turísticos: %d\n", pturisticos);
 printf("Densidade Populacional: %f hab/km²\n", dpopulacional);
-printf("PIB per Capita: %f reais\n\n", pibcapita);*/
+printf("PIB per Capita: %f reais\n\n", pibcapita);
 
 
-/*(printf("Rank: %f\n",rank2);
+(printf("Rank: %f\n",rank2);
 printf("Carta 2\n");
 printf("Estado: B\n");
 printf("Código: B02\n");
@@ -77,44 +77,48 @@ printf("Área: %f km² \n", areaseg);
 printf("PIB %f bilhões de reais \n", pibseg);
 printf("Número de pontos turísticos: %d\n", pturisticosseg);
 printf("Densidade Populacional: %f hab/km²\n", dpopulacionalseg);
-printf("PIB per Capita: %f reais\n\n", pibcapitaseg);)*/
+printf("PIB per Capita: %f reais\n\n", pibcapitaseg);)
 
 printf("Batalha de cartas\n\n");
 
 if(rank1 > rank2 ){
-printf("Rank: Carta 1 venceu (%f)\n", rank1);
-}else 
-printf("Rank: Carta 2 venceu(%f)\n", rank2);}
+printf("Rank: Carta 1 (%s) venceu\n", nome);
+}else {
+printf("Rank: Carta 2 (%s) venceu \n", nomeseg);}
 
 if(populacao > populacaoseg){
-printf("População: Carta 1 venceu (%lu)\n", populacao);
+printf("População: Carta 1 (%lu) venceu \n", populacao);
 }else{
-printf("População: Carta 2 Venceu(%lu)\n",populacaoseg);}
+printf("População: Carta 2 (%lu) venceu \n",populacaoseg);}
 
 if(area > areaseg){
-printf("Área: Carta 1 venceu (%d)\n", area);
+printf("Área: Carta 1 (%d) venceu \n", area);
 }else{
-printf("Áera: Carta 2 venceu(%d)\n", areaseg);}
+printf("Áera: Carta 2 (%d) venceu \n", areaseg);}
 
 if(pib > pibseg){
-printf("PIB: Carta 1 venceu (%d)\n", pib);
+printf("PIB: Carta 1 (%d) venceu \n", pib);
 }else{
-printf("PIB: Carta 2 venceu (%d)\n",pibseg);}
+printf("PIB: Carta 2 (%d) venceu \n",pibseg);}
+
+
 
 if(pturisticos > pturisticosseg){
-printf("Pontos Turísticos: Carta 1 venceu (%d)\n", pturistico);
+printf("Pontos Turísticos: Carta 1 (%d) venceu \n", pturisticos);
 }else{
-printf("Pontos Turísticos: Carta 2 venceu (%d)\n", pturisticoseg);}
+printf("Pontos Turísticos: Carta 2 (%d) venceu \n", pturisticosseg);}
+
+
 
 if(dpopulacional < dpopulacionalseg){
-printf("Densidade Populacional: Carta 1 venceu (%d)\n", dpopulacional);
+printf("Densidade Populacional: Carta 1 (%d) venceu \n", dpopulacional);
 }else{
-printf("Densidade Populacional: Carta 2 venceu (%d)\n", dpopulacionalseg);}
+printf("Densidade Populacional: Carta 2 (%d) venceu \n", dpopulacionalseg);}
 
 if(pibcapita > pibcapitaseg){
-printf("PIB per Capita: Carta 1 venceu (%d)\n", pibcapita);
+printf("PIB per Capita: Carta 1 (%d) venceu\n", pibcapita);
 }else{
-printf("PIB per Capita: Carta 1 venceu (%d)\n", pibcapita);
+printf("PIB per Capita: Carta 2 (%d) venceu\n\n\n", pibcapita);
 }
 
 return 0;
